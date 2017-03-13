935c8493e399288a346cf32fa1464c269efc4e34
Dominic Zenon <dominiczenon@gmail.com>
2017-03-13 00:29:49 -0700

    added

--- scrollbar.js
+++ scrollbar.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
 
     $(".here").click(function(){
-      $(".nav-item").toggle();
+      $(".nav-item").slideToggle(250);
 
     });
+
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
 });
