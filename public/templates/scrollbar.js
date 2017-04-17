b975a8cb8875fd106d73b98b1b62b6b9b30360c7
Dominic Zenon <dominiczenon@gmail.com>
2017-04-17 12:37:22 -0700

    fixed navbar transition and alignment issues

--- public/templates/scrollbar.js
+++ public/templates/scrollbar.js
@@ -1,67 +1,62 @@
 $(document).ready(function(){
 
-    $(".here").click(function(){
-      $(".nav-item").slideToggle(250);
+  $(".here").click(function(){
+    $(".nav-item").slideToggle(250);
 
-    });
+  });
 
-    $('.nav-item').click(function () {
+  $('.nav-item').click(function () {
     $('#toggle-menu').collapse('hide');
-    });
+  });
 
 
 
-    $(function() {
-      $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
         }
+      }
+    });
+  });
+
+
+  var navWhite = document.getElementById("navbar");
+  var navbarlinks = document.querySelectorAll(".nav-item");
+  var navbars = document.getElementById("navigate");
+
+
+  if (window.innerWidth >  760) {
+    window.onscroll = scroll;
+    function scroll() {
+      if (window.pageYOffset >= 100) {
+        navWhite.style.backgroundColor = "rgba(255,255,255, 1)";
+        navWhite.style.boxShadow = "5px 2px 10px";
+      }
+      else {
+        navWhite.style.backgroundColor = "rgba(255,255,255,0)";
+        navWhite.style.boxShadow = "0 0 0";
+      }
+    }
+
+  }
+  else{
+    navWhite.style.backgroundColor = "rgba(255,255,255, 1)";
+  }
+
+
+  if (window.innerWidth <  760) {
+    for (var i = 0; i < navbarlinks.length; i++) {
+      navbarlinks[i].addEventListener("click", function() {
+        navbars.style.display = "none";
       });
-    });
-
-
-    var navWhite = document.getElementById("navbar");
-    var navbarlinks = document.querySelectorAll(".nav-item");
-    var navbars = document.getElementById("navigate");
-
-window.onscroll = scroll;
-if (window.innerWidth >  760) {
-function scroll() {
-  if (window.pageYOffset >= 100) {
-    navWhite.style.backgroundColor = "rgba(255,255,255, 1)";
-    navWhite.style.boxShadow = "5px 2px 10px";
+    }
   }
-  else {
-    navWhite.style.backgroundColor = "rgba(255,255,255,0)";
-    navWhite.style.boxShadow = "0 0 0";
-  }
-}
-}
-else{
-    navWhite.style.backgroundColor = "rgba(255,255,255, 1)";
-}
-
-window.addEventListener("resize", function(){
-  if(document.documentElement.clientWidth>760){
-    navWhite.style.backgroundColor = "rgba(255,255,255, 1)";
-  }
-},false);
-
-
-
-if (window.innerWidth <  760) {
-  for (var i = 0; i < navbarlinks.length; i++) {
-    navbarlinks[i].addEventListener("click", function() {
-      navbars.style.display = "none";
-    });
-  }
-}
 
 });
